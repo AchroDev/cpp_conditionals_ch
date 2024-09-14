35119589c077e23cffde53e973ae3c00ba605a41
AchroDev <achrodev@proton.me>
2024-09-14 14:43:14 -0500

    Added return example and comments

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main()
     for(int i = 0; i < 5; i++)
     {
         if (i > 2)
-            break; // breaks out of the loop, doesn't change anything in this instance.
+            return 0; // forces a return of 0 from the function, once i is > than 2.
         Log("Hello World");
     }
      
-    std::cin.get();
+    std::cin.get(); // this code will never get executed as a return of 0 has already been provided.
 }
