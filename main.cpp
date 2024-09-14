7217b9ed28112fae2fe631cb5f5fe40a56da0a3e
AchroDev <achrodev@proton.me>
2024-09-14 13:34:08 -0500

    Added do-while loop example and comments

--- main.cpp
+++ main.cpp
@@ -3,18 +3,26 @@
 
 int main()
 {
-    
+    // For loop
     for(int i = 0; i < 5; i++)
     {
         Log("this is a for loop");
     }
     
+    
+    // While loop
     int i = 0;
     while(i < 3)
     {
         Log("this is a while loop");
         i++;
     }
+    
+    // Do-while Loop
+    do
+    {
+        
+    } while(i < 5);
     
     Log("Hello World!");    
     std::cin.get();
